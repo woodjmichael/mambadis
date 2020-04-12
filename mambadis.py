e462be6b3fb3d13e96c576596749c0ffc36d3e5d
woodjmichael <woodjmichael@gmail.com>
2020-04-12 16:08:08 +0200

    3.7 - add _nf to numpy float and _ni to numpy int array variable names, _dt to datetime variables, remove some implicit casts

--- mambadis.py
+++ mambadis.py
@@ -26,6 +26,7 @@
 #   3.4 - ** run from command line **, output to csv filename
 #   3.5 - measure runtime, command line run options overwrite defaults
 #   3.6 - simplify offsets, fuel curve lookup table, some small things
+#   3.7 - add _nf to numpy float and _ni to numpy int array variable names, _dt to datetime variables, remove some implicit casts
 
 
 ################################################################################
@@ -56,16 +57,16 @@ class DataClass:
         me.timestep = tstep              # units of seconds
         me.offset = 0                    # in case load and pv data don't begin at same time
         me.datetime = []
-        me.P_kw = np.zeros((length,), dtype=float)
-        me.onlineTime = np.zeros((length,),dtype=int)
-        me.time_to_grid_import_h = np.zeros((length,), dtype=float) # [h]
+        me.P_kw_nf = np.zeros((length,), dtype=float)
+        me.onlineTime_h_ni = np.zeros((length,),dtype=int)
+        me.time_to_grid_import_h_nf = np.zeros((length,), dtype=float) # [h]
         me.code_runtime_s = 0
 
 
     def clear(me):
         me.datetime = []
-        me.P_kw.fill(0)
-        me.time_to_grid_import_h.fill(0)
+        me.P_kw_nf.fill(0)
+        me.time_to_grid_import_h_nf.fill(0)
 
 #
 # Generator
@@ -81,11 +82,11 @@ class GenClass:
         me.fuelcurve_Bcoeff = b                         # [gal/h]
         me.fuelcurve_Ainv = 1/a
         me.fuelTankCapacity = tankCap                   # [gal]
-        me.P_kw = np.zeros((length,), dtype=float)
+        me.P_kw_nf = np.zeros((length,), dtype=float)
         me.fuelConsumed = 0                              # [gal]
 
     def clear(me):
-        me.P_kw.fill(0)
+        me.P_kw_nf.fill(0)
         me.fuelConsumed = 0
 
 
@@ -96,7 +97,7 @@ class GenClass:
             p_final = 0.
         else:
             p_final = 0.
-        me.P_kw[i] = np.array([p_final])
+        me.P_kw_nf[i] = np.array([p_final])
         me.fuel_calc(p_final)
         return p_final
 
@@ -119,13 +120,13 @@ class GenClass:
 class GridClass:
     def __init__(me, Pn_kw, length):
         me.Pn_kw = Pn_kw
-        me.P_kw = np.zeros((length,), dtype=float)
+        me.P_kw_nf = np.zeros((length,), dtype=float)
         me.time_to_import = 0
         me.import_on = 0
         me.offlineCounter = 0
 
     def clear(me):
-        me.P_kw.fill(0)
+        me.P_kw_nf.fill(0)
         me.time_to_import = 0
         me.import_on = 0
 
@@ -137,7 +138,7 @@ class GridClass:
             me.import_on = 1
         elif p_req < 0:
             p_final = np.maximum(p_req, -me.Pn_kw)
-        me.P_kw[index] = p_final
+        me.P_kw_nf[index] = p_final
         return p_final
 
     def timer_tick(me):
@@ -155,25 +156,25 @@ class BattClass:
         me.soc0 = soc0
         me.soc_prev = soc0
         me.timestep = timestep                      # units of seconds
-        me.P_kw = np.zeros((length,), dtype=float)
-        me.soc = np.zeros((length,), dtype=float)
+        me.P_kw_nf = np.zeros((length,), dtype=float)
+        me.soc_nf = np.zeros((length,), dtype=float)
         me.soc_flag = 0                             # binary
 
     def clear(me):
         me.soc_prev = me.soc0
-        me.P_kw.fill(0)
-        me.soc.fill(0)
+        me.P_kw_nf.fill(0)
+        me.soc_nf.fill(0)
         me.soc_flag = 0
 
     def power_request(me, index, p_req):
         if p_req > 0:
-            p_final = np.amin([p_req, me.Pn_kw, me.P_max_soc()])
+            p_final = min(p_req, me.Pn_kw, me.P_max_soc())
         elif p_req < 0:
-            p_final = np.amax([p_req, -me.Pn_kw, me.P_min_soc()])
+            p_final = max(p_req, -me.Pn_kw, me.P_min_soc())
         else:
             p_final = 0.
-        me.soc[index] = me.soc_update(p_final)
-        me.P_kw[index] = p_final
+        me.soc_nf[index] = me.soc_update(p_final)
+        me.P_kw_nf[index] = p_final
         return p_final
 
     def soc_update(me,powerkw):
@@ -280,8 +281,8 @@ def lookup_fuel_curve_coeffs(power):
 def create_synthetic_data():
     pv_oneday = 100*np.array([0., 0., 0., 0., 0., 0., 1., 2., 3., 4., 5., 6., 7., 6., 5., 4., 3., 2., 1., 0., 0., 0., 0., 0.])
     load_oneday = 100*np.array([1., 1., 1., 1., 1., 1., 5., 3., 1., 1., 4., 2., 1., 1., 1., 1., 6., 8., 12., 8., 5., 3., 2., 1.])
-    pv.P_kw = np.concatenate([pv_oneday,pv_oneday,pv_oneday])
-    load.P_kw = np.concatenate([load_oneday,load_oneday,load_oneday])
+    pv.P_kw_nf = np.concatenate([pv_oneday,pv_oneday,pv_oneday])
+    load.P_kw_nf = np.concatenate([load_oneday,load_oneday,load_oneday])
 
 #
 # Import load data
@@ -302,12 +303,12 @@ def import_load_data(site, load_stats):
 
     my_data = np.genfromtxt(filename, delimiter=',')
     md = my_data[1:,1]
-    load_all.P_kw = np.concatenate((md,md),axis=0)
+    load_all.P_kw_nf = np.concatenate((md,md),axis=0)
 
     if load_stats:
-        print('max load [kw] = {:.1f}'.format(np.amax(load_all.P_kw)))
-        print('avg load [kw] = {:.1f}'.format(np.average(load_all.P_kw)))
-        print('min load [kw] = {:.1f}'.format(np.amin(load_all.P_kw)))
+        print('max load [kw] = {:.1f}'.format(np.amax(load_all.P_kw_nf)))
+        print('avg load [kw] = {:.1f}'.format(np.average(load_all.P_kw_nf)))
+        print('min load [kw] = {:.1f}'.format(np.amin(load_all.P_kw_nf)))
 
 #
 # Import solar vector
@@ -335,7 +336,7 @@ def import_pv_data(site):
         else:
             pv[i] = 0
         i += 1
-    pv_all.P_kw = np.concatenate((pv,pv),axis=0)
+    pv_all.P_kw_nf = np.concatenate((pv,pv),axis=0)
 
 #
 # Simulate outage
@@ -356,7 +357,7 @@ def simulate_outage(t_0,L):
     # where in "all load" data this run will end
     m_end = m_0 + L
 
-    load.P_kw = load_all.P_kw[m_0:m_end]
+    load.P_kw_nf = load_all.P_kw_nf[m_0:m_end]
     load.datetime = load_all.datetime[m_0:m_end]
 
     if debug:
@@ -364,7 +365,7 @@ def simulate_outage(t_0,L):
         print('m_0={:d}'.format(m_0) + ' m_end={:d}'.format(m_end))
         print(load.datetime[0])
         print(load.datetime[-1])
-        print(load.P_kw.size)
+        print(load.P_kw_nf.size)
         print(len(load.datetime))
         print('')
 
@@ -376,7 +377,7 @@ def simulate_outage(t_0,L):
     # where in "all load" data this run will end
     n_end = n_0 + L//4                # offset usually 0
 
-    pv.P_kw = pv_all.P_kw[n_0:n_end]
+    pv.P_kw_nf = pv_all.P_kw_nf[n_0:n_end]
     pv.datetime = pv_all.datetime[n_0:n_end]
 
     if debug:
@@ -384,7 +385,7 @@ def simulate_outage(t_0,L):
         print('n_0={:d}'.format(n_0) + ' n_end={:d}'.format(n_end))
         print(pv.datetime[0])
         print(pv.datetime[-1])
-        print(pv.P_kw.size)
+        print(pv.P_kw_nf.size)
         print(len(pv.datetime))
         print('')
 
@@ -409,7 +410,7 @@ def simulate_outage(t_0,L):
         # only increment i_pv every 4 i-increments
         i_pv = i//4
 
-        LSimbalance = load.P_kw[i]      -   pv.P_kw[i_pv]   # load-solar imbalance
+        LSimbalance = load.P_kw_nf[i]      -   pv.P_kw_nf[i_pv]   # load-solar imbalance
 
         battpower = bat.power_request(i,LSimbalance)
 
@@ -425,7 +426,7 @@ def simulate_outage(t_0,L):
             grid.offlineCounter += 1                        # time that microgrid services load
 
         # check energy balance
-        if np.absolute((LSimbalance - bat.P_kw[i] - gen.P_kw[i] - grid.P_kw[i])) > 0.001:
+        if np.absolute((LSimbalance - bat.P_kw_nf[i] - gen.P_kw_nf[i] - grid.P_kw_nf[i])) > 0.001:
             err.energy_balance()
 
     time_to_grid_import = grid.time_to_import/(3600./load.timestep)
@@ -434,18 +435,18 @@ def simulate_outage(t_0,L):
     if vectors_on:
         print('{:},'.format('time') + '{:},'.format('load') + '{:},'.format('pv') + '{:},'.format('b_kw') + '{:},'.format('b_soc') + '{:},'.format('gen') + '{:},'.format('grid') + '{:}'.format('diff'))
         for i in range(L):
-            l=load.P_kw.item(i)
-            p=pv.P_kw.item(i//4)
-            b=bat.P_kw.item(i)
-            s=bat.soc.item(i)
-            g=gen.P_kw.item(i)
-            G=grid.P_kw.item(i)
+            l=load.P_kw_nf.item(i)
+            p=pv.P_kw_nf.item(i//4)
+            b=bat.P_kw_nf.item(i)
+            s=bat.soc_nf.item(i)
+            g=gen.P_kw_nf.item(i)
+            G=grid.P_kw_nf.item(i)
             d=l-p-b-g-G
             print('{:d},'.format(i+1) + '{:.1f},'.format(l) + '{:.1f},'.format(p) + '{:.1f},'.format(b) + '{:.2f},'.format(s) + '{:.1f},'.format(g) + '{:.1f},'.format(G) + '{:.1f}'.format(d))
         print('')
 
     if debug:
-        print('raw sum of batt power [kw]: {:.1f}'.format(np.sum(bat.P_kw).item(0)))
+        print('checksum: {:.1f}'.format(np.sum(bat.P_kw_nf)))
 
     return time_to_grid_import
 
@@ -459,7 +460,7 @@ def simulate_outage(t_0,L):
 #
 ################################################################################
 
-t_begin = dt.datetime.now()
+t_begin_dt = dt.datetime.now()
 
 err =   FaultClass()
 
@@ -562,13 +563,13 @@ for i in range(runs):
 
     results.datetime.append(dt.datetime(2019,1,1) + dt.timedelta(hours=h))
 
-    results.time_to_grid_import_h[i] = simulate_outage(t0,L)
+    results.time_to_grid_import_h_nf[i] = simulate_outage(t0,L)
 
-    results.onlineTime[i] = grid.offlineCounter/4.
+    results.onlineTime_h_ni[i] = grid.offlineCounter/4.
 
-t_end = dt.datetime.now()
-t_elapsed = t_end - t_begin
-results.code_runtime_s = t_elapsed.total_seconds()
+t_end_dt = dt.datetime.now()
+t_elapsed_dt = t_end_dt - t_begin_dt
+results.code_runtime_s = t_elapsed_dt.total_seconds()
 
 
 
@@ -593,29 +594,29 @@ if output_file_on:
         output.writerow([])
         output.writerow(['Run','Outage Start', 'Time to First Failure [h]', 'Cumulative Operating Time [h]'])
         for i in range(runs):
-            output.writerow([i+1,results.datetime[i],results.time_to_grid_import_h[i],results.onlineTime[i]])
+            output.writerow([i+1,results.datetime[i],results.time_to_grid_import_h_nf[i],results.onlineTime_h_ni[i]])
 
 # plots
 if plots_on:
-    t = np.arange(1., L+1., 1.)
+    t_ni = np.arange(1., L+1., 1.)
 
     fig, ax1 = plt.subplots(figsize=(20,10))
 
 
-    bat_p_kw_pos = np.clip(bat.P_kw,0,10000)
-    bat_p_kw_neg = -1*np.clip(bat.P_kw,-10000,0)
+    bat_p_kw_pos_nf = np.clip(bat.P_kw_nf,0,10000)
+    bat_p_kw_neg_nf = -1*np.clip(bat.P_kw_nf,-10000,0)
 
     ax2 = ax1.twinx()
-    ax1.plot(t,pv.P_kw,                             'y',    label='PV')
-    ax1.plot(t,pv.P_kw + bat_p_kw_pos,              'b',    label='battdis+PV')
-    ax1.plot(t,pv.P_kw + bat_p_kw_pos + gen.P_kw,   'g',    label='gen+battdis+PV')
-    ax1.plot(t,load.P_kw,                           'k',    label='load')
-    ax1.plot(t,load.P_kw + bat_p_kw_neg,            'k-*',  label='load+battchg')
-    ax2.plot(t,bat.soc,                             'm-',   label='soc', marker='')
+    ax1.plot(t_ni, pv.P_kw_nf,                             'y',    label='PV')
+    ax1.plot(t_ni, pv.P_kw_nf + bat_p_kw_pos_nf,              'b',    label='battdis+PV')              # implicit cast? x3
+    ax1.plot(t_ni, pv.P_kw_nf + bat_p_kw_pos_nf + gen.P_kw_nf,   'g',    label='gen+battdis+PV')
+    ax1.plot(t_ni, load.P_kw_nf,                           'k',    label='load')
+    ax1.plot(t_ni, load.P_kw_nf + bat_p_kw_neg_nf,            'k-*',  label='load+battchg')
+    ax2.plot(t_ni, bat.soc_nf,                             'm-',   label='soc', marker='')
 
-    ax1.fill_between(t, 0, pv.P_kw, facecolor='yellow')#, interpolate=True)
-    ax1.fill_between(t, pv.P_kw, pv.P_kw+bat_p_kw_pos, facecolor='blue')#, interpolate=True)
-    ax1.fill_between(t, pv.P_kw+bat_p_kw_pos, pv.P_kw+bat_p_kw_pos+gen.P_kw, facecolor='green')#, interpolate=True)
+    ax1.fill_between(t_ni, 0, pv.P_kw_nf, facecolor='yellow')#, interpolate=True)
+    ax1.fill_between(t_ni, pv.P_kw_nf, pv.P_kw_nf+bat_p_kw_pos_nf, facecolor='blue')#, interpolate=True)
+    ax1.fill_between(t_ni, pv.P_kw_nf+bat_p_kw_pos_nf, pv.P_kw_nf + bat_p_kw_pos_nf + gen.P_kw_nf, facecolor='green')#, interpolate=True)
     ax1.set_ylabel('between y1 and 0')
 
     ax1.set_xlabel('h')
